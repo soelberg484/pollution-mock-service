d5d4ab2b712398850d08aed3d6efe044ff04305c
Johnny Chen <s165543@win.dtu.dk>
2020-04-19 12:29:47 +0200

    ini

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App: React.FC = () => {
+const [activeStream, setActiveStream] = useState(false)
+const [aqiMock, setAqiMock] = useState(0)
+const [streamMsg, setStreamMsg] = useState("")
+
+const generateRandomNumber = (interval: number) => {
+    if(interval >= 0){
+    const randomNo = Math.random() * 400
+    console.log(randomNo)
+    setAqiMock(randomNo)
+    setTimeout(() => generateRandomNumber(interval - 1), 1000)
+    }
+    else {
+      setStreamMsg("Stream stopping...")
+      setTimeout(() => {
+        setActiveStream(false)},
+        3000)
+    }
 }
 
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    setStreamMsg('Stream is running...  (' + aqiMock +')');
+  }, [aqiMock])
+  return (
+    <div>
+      <h1>Pollution mock service</h1>
+      {activeStream && <h3>{streamMsg}</h3>}
+      <button onClick={() => {
+        setActiveStream(true);
+        generateRandomNumber(10)}}>Start stream</button>
+      <button onClick={() => setActiveStream(false)}>Stop stream</button>
+    </div>
+  )
+}
 export default App;
